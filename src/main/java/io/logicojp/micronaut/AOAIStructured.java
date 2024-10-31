13a92cda0b780d5eac7a153e6c3d169339fa5f05
anishi1222 <a_nishi-anarch@bc4.so-net.ne.jp>
2024-10-31 11:01:58 +0900

    In the case of using JSON Schema file

--- src/main/java/io/logicojp/micronaut/AOAIStructured.java
+++ src/main/java/io/logicojp/micronaut/AOAIStructured.java
@@ -13,7 +13,6 @@ import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
-import io.micronaut.http.annotation.QueryValue;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
 import jakarta.inject.Inject;
@@ -21,6 +20,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 @Controller("/aoai")
@@ -30,7 +31,7 @@ public class AOAIStructured {
             My name is John and I was born in 1970.
             """;
     final String systemMessage = """
-            ユーザーの発言内容から名前と年齢を抽出して JSON 形式に整形してください。
+            ユーザーの発言内容から、現在日時を基準にした年齢と名前を抽出して JSON 形式に整形してください。
             """;
 
     @Property(name ="azure.openai.deploymentName")
@@ -43,43 +44,79 @@ public class AOAIStructured {
         this.aoaiClientFactory = aoaiClientFactory;
     }
 
-@Get("/query1")
-@Produces(MediaType.APPLICATION_JSON)
-@ExecuteOn(TaskExecutors.IO)
-public Person querySample(@QueryValue(value = "u", defaultValue = userMessage) String _userMessage) throws IOException {
+    @Get("/query1")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ExecuteOn(TaskExecutors.IO)
+    public Person querySample1() throws IOException {
 
-    Logger logger = LoggerFactory.getLogger(AOAIStructured.class);
-    // Initialize ObjectMapper and SchemaFactoryWrapper
-    ObjectMapper objectMapper = new ObjectMapper();
-    SchemaFactoryWrapper visitor = new SchemaFactoryWrapper();
+        Logger logger = LoggerFactory.getLogger(AOAIStructured.class);
+        // Initialize ObjectMapper and SchemaFactoryWrapper
+        ObjectMapper objectMapper = new ObjectMapper();
+        SchemaFactoryWrapper visitor = new SchemaFactoryWrapper();
 
-    // Generate JSON schema
-    try {
-        objectMapper.acceptJsonFormatVisitor(objectMapper.constructType(Person.class), visitor);
-    } catch (JsonMappingException e) {
-        throw new RuntimeException(e);
+        // Generate JSON schema
+        try {
+            objectMapper.acceptJsonFormatVisitor(objectMapper.constructType(Person.class), visitor);
+        } catch (JsonMappingException e) {
+            throw new RuntimeException(e);
+        }
+        JsonSchema jsonSchema = visitor.finalSchema();
+
+        OpenAIClient client = aoaiClientFactory.getAOAIClient();
+        ChatCompletions chatCompletions
+            = client.getChatCompletions(deploymentName,
+                new ChatCompletionsOptions(
+                    Arrays.asList(
+                        new ChatRequestSystemMessage(systemMessage),
+                        new ChatRequestUserMessage(userMessage)
+                    )
+                )
+                .setResponseFormat(
+                    new ChatCompletionsJsonSchemaResponseFormat(
+                        new ChatCompletionsJsonSchemaResponseFormatJsonSchema("Person")
+                            .setStrict(false)
+                            .setDescription("Fetch age and name from response.")
+                                .setSchema(BinaryData.fromObject(jsonSchema))
+                    )
+                )
+            );
+
+        return objectMapper.readValue(chatCompletions.getChoices().getFirst().getMessage().getContent(), Person.class);
     }
-    JsonSchema jsonSchema = visitor.finalSchema();
 
-    OpenAIClient client = aoaiClientFactory.getAOAIClient();
-    ChatCompletions chatCompletions
-        = client.getChatCompletions(deploymentName,
-            new ChatCompletionsOptions(
-                Arrays.asList(
-                    new ChatRequestSystemMessage(systemMessage),
-                    new ChatRequestUserMessage(_userMessage)
+    @Get("/query2")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ExecuteOn(TaskExecutors.IO)
+    public Person querySample2() throws IOException {
+
+        Logger logger = LoggerFactory.getLogger(AOAIStructured.class);
+        // Initialize ObjectMapper and SchemaFactoryWrapper
+        // JSON Schema file should be located in the same directory as the application JAR file.
+        Path jsonSchemaPath = Paths.get(Application.JSONSchemaFile);
+        OpenAIClient client = aoaiClientFactory.getAOAIClient();
+        ChatCompletions chatCompletions
+                = client.getChatCompletions(deploymentName,
+                new ChatCompletionsOptions(
+                        Arrays.asList(
+                                new ChatRequestSystemMessage(systemMessage),
+                                new ChatRequestUserMessage(userMessage)
+                        )
                 )
-            )
-            .setResponseFormat(
-                new ChatCompletionsJsonSchemaResponseFormat(
-                    new ChatCompletionsJsonSchemaResponseFormatJsonSchema("Person")
-                        .setStrict(false)
-                        .setDescription("Fetch age and name from response.")
-                            .setSchema(BinaryData.fromObject(jsonSchema))
-                )
-            )
+                        .setResponseFormat(
+                                new ChatCompletionsJsonSchemaResponseFormat(
+                                        new ChatCompletionsJsonSchemaResponseFormatJsonSchema("Person")
+                                                .setStrict(true)
+                                                .setDescription("Fetch age and name from response.")
+                                                .setSchema(BinaryData.fromFile(jsonSchemaPath))
+                                )
+                        )
         );
 
-    return objectMapper.readValue(chatCompletions.getChoices().getFirst().getMessage().getContent(), Person.class);
-}
+        return new ObjectMapper()
+                .readValue(chatCompletions.getChoices()
+                                .getFirst()
+                                .getMessage()
+                                .getContent(),
+                            Person.class);
+    }
 }
--- src/main/java/io/logicojp/micronaut/Application.java
+++ src/main/java/io/logicojp/micronaut/Application.java
@@ -3,8 +3,19 @@ package io.logicojp.micronaut;
 import io.micronaut.runtime.Micronaut;
 
 public class Application {
+    static String JSONSchemaFile;
 
     public static void main(String... args) {
+        if(args.length == 0) {
+            JSONSchemaFile = "SampleSchema.json";
+        }
+        else if(args.length == 1) {
+            JSONSchemaFile = args[0];
+        }
+        else {
+            System.out.println("Usage: java -jar <JAR file> <JSON Schema file>");
+            System.exit(1);
+        }
         Micronaut.run(Application.class, args);
     }
 }
